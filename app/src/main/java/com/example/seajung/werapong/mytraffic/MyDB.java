4fd59bb1ebc612d0f86572a002e65f5705633f25
Werapong <werapong.jung@gmail.com>
2017-05-20 15:04:39 +0700

    Finish 20/05/2017

--- app/src/main/java/com/example/seajung/werapong/mytraffic/MyDB.java
+++ app/src/main/java/com/example/seajung/werapong/mytraffic/MyDB.java
@@ -5,4 +5,24 @@ package com.example.seajung.werapong.mytraffic;
  */
 
 public class MyDB {
+
+    //Explicit เริ่มประกาศตัวแปร
+    //ต้องมี 3 ก้อนเสมอ
+    //1.Access เช่น Public, Private
+    //2.Type
+    //3.Name
+    private String[] titlesStrings = new String[]{"หัวข้อที่ 1", "หัวข้อที่ 2", "หัวข้อที่ 3", "หัวข้อที่ 4",
+            "หัวข้อที่ 5", "หัวข้อที่ 6", "หัวข้อที่ 7", "หัวข้อที่ 8", "หัวข้อที่ 9", "หัวข้อที่ 10", "หัวข้อที่ 11", "หัวข้อที่ 12", "หัวข้อที่ 13",
+            "หัวข้อที่ 14", "หัวข้อที่ 15", "หัวข้อที่ 16", "หัวข้อที่ 17", "หัวข้อที่ 18", "หัวข้อที่ 19", "หัวข้อที่ 20",};
+    private String[] detailStrings = new String;
+
+    public String[] getTitlesStrings() {
+        return titlesStrings;
+    }
+
+    public String[] getDetailStrings() {
+        String[] resulStrings;
+        return detailStrings;
+    }
 }   // Main Class คราสหลัก
+
